d3b76d42bb93f7637824f2cf640ae99cedd7b3fa
RashThePay <arashp999@gmail.com>
2024-03-10 18:47:09 +0330

    init auth

--- src/app/layout.js
+++ src/app/layout.js
@@ -18,7 +18,6 @@ export default function RootLayout({ children }) {
             <Script id="yektanet">
             {`!function(e,t,n){e.yektanetAnalyticsObject=n,e[n]=e[n]||function(){e[n].q.push(arguments)},e[n].q=e[n].q||[];var a=t.getElementsByTagName("head")[0],r=new Date,c="https://cdn.yektanet.com/superscript/h1dloEgK/native-royaai.ir-36735/yn_pub.js?v="+r.getFullYear().toString()+"0"+r.getMonth()+"0"+r.getDate()+"0"+r.getHours(),s=t.createElement("link");s.rel="preload",s.as="script",s.href=c,a.appendChild(s);var l=t.createElement("script");l.async=!0,l.src=c,a.appendChild(l)}(window,document,"yektanet");`}
             </Script>
-            
       <body className={vazir.className}>
         <Providers>
           <Navbar/>
